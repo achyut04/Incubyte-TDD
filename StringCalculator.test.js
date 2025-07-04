022a7b9b07dcfb67b73e1187600f43b69181d3c9
achyut04 <achyutsavaliya4@gmail.com>
2025-07-04 19:39:43 +0530

    [Step-1] Failing test for 1 number in input

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -1,5 +1,9 @@
-const Add = require('./StringCalculator');
+const Add = require("./StringCalculator");
 
 test("returns 0 for an empty string", () => {
   expect(Add("")).toBe(0);
+});
+
+test("returns the value itself if there is only 1 number in input", () => {
+  expect(Add("1")).toBe(1);
 });
