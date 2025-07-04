d9ad2289425d4c59ae227332e57dbe234422958e
achyut04 <achyutsavaliya4@gmail.com>
2025-07-04 19:50:23 +0530

    [Step-1] Failing test for 2 comma sepearted numbers in input

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -7,3 +7,8 @@ test("returns 0 for an empty string", () => {
 test("returns the value itself if there is only 1 number in input", () => {
   expect(Add("1")).toBe(1);
 });
+
+test("returns sum of 2 numbers for 2 comma separated numbers in input", () => {
+    expect(Add("1,2")).toBe(3);
+    expect(Add("5,7")).toBe(12);
+});
