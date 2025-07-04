31252527f4c81b31c75e1fcb4fd8fe704e6d6f16
achyut04 <achyutsavaliya4@gmail.com>
2025-07-04 22:27:38 +0530

    [Step-4] Passed test for custom delimiters

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,14 +1,20 @@
-function Add(numbers){
-    if(numbers == "")
-        return 0;
-    let values = numbers.split(/[\n,]/);
-    const sum = values.reduce((total, val) => {
-        return total + parseInt(val);
-    }, 0);
+function Add(input) {
+  if (input == "") return 0;
+  let delimiter = /[\n,]/;
+  let nums = input;
+  if (input.startsWith("//")) {
+    const delimiterEndIndex = input.indexOf("\n");
+    delimiter = input.substring(2, delimiterEndIndex);
+    nums = input.substring(delimiterEndIndex + 1);
+  }
+  let values = nums.split(delimiter);
+  const sum = values.reduce((total, val) => {
+    return total + parseInt(val);
+  }, 0);
 
-    if(values.length === 0) return parseInt(numbers);
+  if (values.length === 0) return parseInt(nums);
 
-    return sum;
+  return sum;
 }
 
 module.exports = Add;
